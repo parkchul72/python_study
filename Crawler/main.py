b73bc63065a38729cea07b66941e9a916c385791
Fred <parkchul@nate.com>
2016-04-21 14:30:59 +0900

    #2 commit

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,6 +1,10 @@
 import os
 
+# Each website you want to crawl is a seperate project(folder)
 def create_project_dir(directory):
     if not os.path.exists(directory):
         print("Creating Project " + directory)
         os.makedirs(directory)
+
+# Create queue and crawled files(if not created)
+def crate_data_files(project_name, base_url):

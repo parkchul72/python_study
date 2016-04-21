5ef3150473802f3803ff0eefd07305926f4cb29b
Fred <parkchul@nate.com>
2016-04-21 15:28:30 +0900

    #2 commit

--- Scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ Scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,14 +1,34 @@
 import scrapy
 from tutorial.items import DmozItem
 
+
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
+    """
     start_urls = [
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
+    """
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/"
+    ]
 
+    def parse(self, response):
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+
+"""
     #.3
     def parse(self, response):
         for sel in response.xpath('//ul/li'):
@@ -16,8 +36,8 @@ class DmozSpider(scrapy.Spider):
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
-            yield
-"""
+            yield item
+
     #1
     def parse(self, response):
         filename = response.url.split("/")[-2] + '.html'
